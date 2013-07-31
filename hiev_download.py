ec51243285250ce645e01708aab01e8895add93f
Gerard Devine <gerarddevine@gmail.com>
2013-07-31 16:44:35 +1000

    Separate out the config file

--- hiev_download.py
+++ hiev_download.py
@@ -7,30 +7,39 @@ Date: July 2013
 
 import json
 import urllib2
+import yaml
 
 
-# --Set up my request
-request_url     = "http://w0297.uws.edu.au/data_files/api_search"
-request_headers = {'Content-Type' : 'application/json; charset=UTF-8','X-Accept': 'application/json'}
-request_data    = json.dumps({"auth_token": "<API Key>", "variables" : ["AirTC"]})
+# --Read in settings from config file
+stram = open("config.yaml", "r")
+doc = yaml.load_all(stram)
 
 
-# --Handle the returned response from the HIEv server
-request  = urllib2.Request(request_url, request_data, request_headers)
-response = urllib2.urlopen(request)
-js = json.load(response)
+for x in doc:
+    api_token = x["api_token"]
 
-
-# --For each element returned pass the url to the download API and download
-for item in js:
-    download_url = item["url"]+'?'+'auth_token=PSEsmywGqiEMyVni5r54'
-    request  = urllib2.Request(download_url)
-    f = urllib2.urlopen(request)
-
-    # Open a local file for writing 
-    with open(item["filename"], "wb") as local_file:
-        local_file.write(f.read())
-        
-#TODO Print a summary of activity to a local log file
-#TODO Abstract filter variables out to a configuration file
-print 'Total files = %s' %len(js)
+    # --Set up my request
+    request_url     = "http://w0297.uws.edu.au/data_files/api_search"
+    request_headers = {'Content-Type' : 'application/json; charset=UTF-8','X-Accept': 'application/json'}
+    request_data    = json.dumps({"auth_token": api_token, "variables" : ["AirTC"]})
+    
+    
+    # --Handle the returned response from the HIEv server
+    request  = urllib2.Request(request_url, request_data, request_headers)
+    response = urllib2.urlopen(request)
+    js = json.load(response)
+    
+    
+    # --For each element returned pass the url to the download API and download
+    for item in js:
+        download_url = item["url"]+'?'+'auth_token=PSEsmywGqiEMyVni5r54'
+        request  = urllib2.Request(download_url)
+        f = urllib2.urlopen(request)
+    
+        # Open a local file for writing 
+        with open(item["filename"], "wb") as local_file:
+            local_file.write(f.read())
+            
+    #TODO Print a summary of activity to a local log file
+    #TODO Abstract filter variables out to a configuration file
+    print 'Total files = %s' %len(js)
