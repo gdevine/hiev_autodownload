faffac50a1abb94f69d208b158ad08ee05a3bc86
Gerard Devine <gerarddevine@gmail.com>
2013-08-01 11:31:19 +1000

    Refactor settings.yaml

--- hiev_download.py
+++ hiev_download.py
@@ -1,43 +1,51 @@
 '''
-Python script to perform a HIEv API filter search and then download files based on the returned urls
+Python script to perform a HIEv API filter search and then download of files 
+based on the returned urls. This script is informed by a config.yaml file.
 
 Author: Gerard Devine
 Date: July 2013
 '''
 
+import os
 import json
 import urllib2
 import yaml
 
 
-# --Read in settings from config file
-stram = open("config.yaml", "r")
-doc = yaml.load_all(stram)
+# --Read the yaml settings file into python 
+stram = open("settings.yaml", "r")
+settings = yaml.load(stram)
 
+# --Parse global values
+request_url = settings['request_url']
+api_token = settings['api_token']
 
-for x in doc:
-    api_token = x["api_token"]
+# --Iterate over each individual entry in the params set
+for entry in settings['params']:
+    variables = entry['variables']
+    dest_dir = entry['dest_dir']
 
-    # --Set up my request
-    request_url     = "http://w0297.uws.edu.au/data_files/api_search"
-    request_headers = {'Content-Type' : 'application/json; charset=UTF-8','X-Accept': 'application/json'}
-    request_data    = json.dumps({"auth_token": api_token, "variables" : ["AirTC"]})
-    
+    # --Set up the http request
+    request_headers = {'Content-Type' : 'application/json; charset=UTF-8', 'X-Accept': 'application/json'}
+    request_data    = json.dumps({'auth_token': api_token, 'variables' : variables})
     
     # --Handle the returned response from the HIEv server
     request  = urllib2.Request(request_url, request_data, request_headers)
     response = urllib2.urlopen(request)
     js = json.load(response)
     
-    
     # --For each element returned pass the url to the download API and download
     for item in js:
-        download_url = item["url"]+'?'+'auth_token=PSEsmywGqiEMyVni5r54'
+        download_url = item['url']+'?'+'auth_token=%s' %api_token
         request  = urllib2.Request(download_url)
         f = urllib2.urlopen(request)
     
-        # Open a local file for writing 
-        with open(item["filename"], "wb") as local_file:
+        # --Create the directory to write to if not already existing 
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        
+        # --Write the file
+        with open(os.path.join(dest_dir, item['filename']), 'wb') as local_file:
             local_file.write(f.read())
             
     #TODO Print a summary of activity to a local log file
